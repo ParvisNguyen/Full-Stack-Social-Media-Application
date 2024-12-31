f82a91372224063fc19a0acca7308b2292ebdf4b
Parvis Nguyen <parvis_nguyen@hotmail.com>
2024-12-31 13:37:35 -0500

    Adding sequelize

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express();
+const express = require('express') //Express is a Node.js web application framework
+const app = express(); // Create an Express application
 
-app.listen(3001, () => {
-    console.log("Server running on port 3001")
-});git 
+app.listen(3001, () => { // Listen on port 3001
+    console.log("Server running on port 3001") // Log a message to the console
+});
+ 
