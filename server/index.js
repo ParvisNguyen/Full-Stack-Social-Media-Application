20db5f5cf618d9816e6aac0e7802b8f8abdcf0a0
Parvis Nguyen <parvis_nguyen@hotmail.com>
2024-12-31 22:54:46 -0500

    MySQL configuration

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express') //Express is a Node.js web application framework
 const app = express(); // Create an Express application
 
-app.listen(3001, () => { // Listen on port 3001
-    console.log("Server running on port 3001") // Log a message to the console
+const db = require('./models'); // Import the models
+
+
+db.sequelize.sync().then(() => { // Sync the models
+    app.listen(3001, () => { // Listen on port 3001
+        console.log("Server running on port 3001") // Log a message to the console
+    });
 });
  
