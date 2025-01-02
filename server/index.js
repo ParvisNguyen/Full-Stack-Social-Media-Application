99d082b8d1cefd3709456fef943d93f0a426a0aa
Parvis Nguyen <parvis_nguyen@hotmail.com>
2025-01-02 00:07:35 -0500

    Adding router

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express(); // Create an Express application
 const db = require('./models'); // Import the models
 
 
+
 db.sequelize.sync().then(() => { // Sync the models
     app.listen(3001, () => { // Listen on port 3001
         console.log("Server running on port 3001") // Log a message to the console
---
+++ server/routes/Posts.js
@@ -0,0 +1,4 @@
+const express = require('express'); // Import the express module
+const router = express.Router(); // Create a new router
+
+module.exports = router; // Export the router
