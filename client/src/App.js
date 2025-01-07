658f38544b020b1d405f76a2b72725731ae41fde
Parvis Nguyen <parvis_nguyen@hotmail.com>
2025-01-07 13:33:37 -0500

    Designed a webpage

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,15 @@ function App() {
     return (
     <div className="App">
       {listOfPosts.map((value, key) => {
-        return <div> {value.title} </div>;
-      })} 
-    </div>
+        return (
+          <div className="post">
+            <div className="title">{value.title} </div>     
+            <div className="body">{value.postText} </div>
+            <div className="footer">{value.username} </div>
+        </div>
+      );
+    })} 
+  </div>
   );
 }
 
