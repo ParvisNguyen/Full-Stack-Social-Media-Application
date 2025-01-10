cb287dc795f350a5342d9be5d8fb5d40f41a5fa6
Parvis Nguyen <parvis_nguyen@hotmail.com>
2025-01-10 15:33:27 -0500

    Fixing Post Page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,10 +14,10 @@ function Home() {
     }, []);
 
     return (
-        <div className="postPage">
+        <div>
             {listOfPosts.map((value, key) => {
                 return (
-                    <div className="post" onClick={() => {navigate.push(`/post/${value.id}`)}}>
+                    <div className="post" onClick={() => {navigate(`/post/${value.id}`)}}>
                         <div className="title">{value.title} </div>
                         <div className="body">{value.postText} </div>
                         <div className="footer">{value.username} </div>
--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { use, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 function Post() {
@@ -10,18 +10,20 @@ function Post() {
         axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
             setPostObject(response.data);
         });
-    });
+    }, [id]); // Add the dependency array with 'id'
+
     return (
         <div className="postPage">
             <div className="leftSide">
-                <div className="title">{postObject.title}</div>
-                <div className="postText">{postObject.postText}</div>
-                <div className="footer">{postObject.username}</div>
+                <div className="post" id="individual">
+                    <div className="title">{postObject.title}</div>
+                    <div className="body">{postObject.postText}</div>
+                    <div className="footer">{postObject.username}</div>
+                </div>
             </div>
             <div className="rightSide">Comment Section</div>
         </div>
-    )
+    );
 }
-
 
 export default Post;
