0ef39f001a28159d73c74aab8a8ca91f4a8b90b5
Parvis Nguyen <parvis_nguyen@hotmail.com>
2025-01-15 17:09:35 -0500

    Added a full functional comment section

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,16 +1,33 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Post() {
     let { id } = useParams();
     const [postObject, setPostObject] = useState({});
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState("");
 
     useEffect(() => {
         axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
             setPostObject(response.data);
         });
-    }, [id]); // Add the dependency array with 'id'
+        axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
+            setComments(response.data);
+        });
+    }, [id]);
+
+    const addComment = () => {
+        axios.post("http://localhost:3001/comments", {
+            commentBody: newComment,
+            PostId: id
+        })
+            .then((response) => {
+                const commentToAdd = { commentBody: newComment };
+                setComments([...comments, commentToAdd]);
+                setNewComment("");
+            });
+    };
 
     return (
         <div className="postPage">
@@ -21,7 +38,26 @@ function Post() {
                     <div className="footer">{postObject.username}</div>
                 </div>
             </div>
-            <div className="rightSide">Comment Section</div>
+            <div className="rightSide">
+                <div className="addCommentContainer">
+                    <input type="text"
+                        placeholder="Your Comment Here"
+                        autoComplete="off"
+                        value = {newComment}
+                        onChange={(event) => {
+                            setNewComment(event.target.value)
+                        }}
+                    />
+                    <button onClick={addComment}> Comment </button>
+                </div>
+                <div className="listOfComments">
+                    {comments.map((comment, key) => {
+                        return <div key={key} className="comment">
+                            {comment.commentBody}
+                        </div>
+                    })}
+                </div>
+            </div>
         </div>
     );
 }
